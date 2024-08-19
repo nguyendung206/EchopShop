aa5713f11c02e9c632bda1a31bf621ca5f4d3ed0
nguyendung206 <moto2062002@gmail.com>
2024-08-19 16:44:05 +0700

    setup

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -1,21 +1,12 @@
 <?php
 
 namespace App\Http\Controllers\Admin;
-
 use App\Http\Controllers\Controller;
-use GuzzleHttp\Psr7\Request;
+
 
 class AuthController extends Controller
 {
-    public function index()
-    {
-        return view('admin.index', [
-            'title' => 'Đăng nhập hệ thống'
-        ]);
-    }
-
-    public function login(Request $request)
-    {
-        
+    public function index() {
+        return view('admin.index');
     }
 }
