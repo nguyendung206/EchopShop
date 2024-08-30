29fc55fc8d674e98671d6e5fe0ce613edaf4f1b3
chienphan <chienphan230702@gmail.com>
2024-08-30 16:40:52 +0700

    auth v1

--- app/Services/ImageService.php
+++ app/Services/ImageService.php
@@ -11,11 +11,13 @@ class ImageService
         if ($file) {
             $fileName = time() . '-' . $file->getClientOriginalName();
             $file->storeAs($path, $fileName, 'public');
+
             return $fileName;
         }
 
         return $defaultImage;
     }
+
 
     public function deleteImage($fileName, $path = 'upload/product', $defaultImage = 'noproduct.png')
     {
@@ -23,6 +25,26 @@ class ImageService
         if (file_exists($filePath) && $fileName !== $defaultImage) {
             unlink($filePath);
         }
+    }
 
+
+    public function uploadMultipleImages($files, $path = 'upload/product')
+    {
+        $fileNames = [];
+        if ($files && is_array($files)) {
+            foreach ($files as $file) {
+                $fileName = $this->uploadImage($file, $path);
+                $fileNames[] = $fileName;
+            }
+        }
+        return $fileNames;
+    }
+    public function deleteMultipleImages($fileNames, $path = 'upload/product', $defaultImage = 'noproduct.png')
+    {
+        if ($fileNames && is_array($fileNames)) {
+            foreach ($fileNames as $fileName) {
+                $this->deleteImage($fileName, $path, $defaultImage);
+            }
+        }
     }
 }
