097051662203a5d3998e514794a707676b52245b
chienphan <chienphan230702@gmail.com>
2024-09-05 18:17:18 +0700

    merge main

--- routes/web.php
+++ routes/web.php
@@ -2,7 +2,6 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Web\AuthController;
-use App\Http\Controllers\Web\HomeController;
 
 require __DIR__ . '/admin.php';
 /*
@@ -16,22 +15,21 @@ require __DIR__ . '/admin.php';
 |
 */
 
-// Route::get('/', function () {
+Route::get('/', function () {
 
-//     return view('welcome');
-// });
-Route::get('/',[HomeController::class, 'index']);
-Route::get('/getBrand', [HomeController::class, 'getBrand'])->name('web.getBrand');
+    return view('welcome');
+});
 
-Route::get('/web/login', [AuthController::class, 'index'])->name('web.login');
-Route::post('/web/login', [AuthController::class, 'login'])->name('web.authentication');
+Route::get('/login', [AuthController::class, 'index'])->name('web.login');
+Route::post('/login', [AuthController::class, 'login'])->name('web.authentication');
 
-Route::post('/web/register', [AuthController::class, 'store'])->name('web.register.store');
-Route::get('/web/register', [AuthController::class, 'register'])->name('web.register');
-Route::post('/web/district', [AuthController::class, 'getDistrict'])->name('web.district');
-Route::post('/web/ward', [AuthController::class, 'getWard'])->name('web.ward');
+Route::post('/register', [AuthController::class, 'store'])->name('web.register.store');
+Route::get('/register', [AuthController::class, 'register'])->name('web.register');
+Route::post('/district', [AuthController::class, 'getDistrict'])->name('web.district');
+Route::post('/ward', [AuthController::class, 'getWard'])->name('web.ward');
 
-Route::get('/web/forgotPassword', [AuthController::class, 'forgotPassword'])->name('web.forgotPassword');
-Route::post('/web/forgotPassword', [AuthController::class, 'handleForgotPassword'])->name('web.handleForgotPassword');
-Route::get('/web/ResetPassword/{token}', [AuthController::class, 'resetPassword'])->name('web.resetPassword');
-Route::post('/web/ResetPassword/{token}', [AuthController::class, 'handleResetPassword'])->name('web.handleResetPassword');
+Route::get('/forgotPassword', [AuthController::class, 'forgotPassword'])->name('web.forgotPassword');
+Route::post('/forgotPassword', [AuthController::class, 'handleForgotPassword'])->name('web.handleForgotPassword');// gửi mail
+Route::get('/pinAuthentication/{token}', [AuthController::class, 'indexPinAuthentication'])->name('web.pinAuthentication');  // url bên mail
+Route::post('/pinCode/{token}',[AuthController::class, 'checkPinCode'])->name('web.pinCode');
+Route::post('/resetPassword/{token}', [AuthController::class, 'handleResetPassword'])->name('web.handleResetPassword');
