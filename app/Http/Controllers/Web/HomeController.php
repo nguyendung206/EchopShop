9f510ee78e55977240dad44452df27fcbff9f995
chienphan <chienphan230702@gmail.com>
2024-09-09 15:13:25 +0700

    secondhandProduct

--- app/Http/Controllers/Web/HomeController.php
+++ app/Http/Controllers/Web/HomeController.php
@@ -9,12 +9,13 @@ use App\Models\Category;
 use App\Models\Brand;
 use App\Models\Product;
 use App\Models\Banner;
+use App\Enums\TypeProduct;
 
 class HomeController extends Controller
 {
     public function index(Request $request) {
         $banners = Banner::query()->where('status', 1)->orderBy('display_order', 'asc')->limit(4)->get();
-        $secondhandProducts = Product::query()->where('status', 1)->limit(8)->get();
+        $secondhandProducts = Product::query()->where('status', 1)->where('type', TypeProduct::SECONDHAND->value)->limit(8)->get();
         
         return view('web.home.home', compact('banners', 'secondhandProducts'));
     }
--- resources/views/web/home/home.blade.php
+++ resources/views/web/home/home.blade.php
@@ -102,7 +102,7 @@
             <div class="row">
                 @foreach($secondhandProducts as $product)
                 <div class="col-sm-4 col-md-4 col-lg-3 text-center col-6 py-3 product-item">
-                    <img class="product-img" src="{{ asset('/img/image/secondhand1.jpeg') }} " alt="">
+                    <img class="product-img" src="{{ getImage($product->photo) }} " alt="">
                     <i class="fa-regular fa-heart fa-heart-home"></i>
                     <p class="product-name pt-2">{{$product->name}}</p>
                     <p class="price color-B10000 pt-2">{{$product->price}} đ</p>
