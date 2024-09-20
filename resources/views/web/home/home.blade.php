4a9ab5ada6f1fd194f9d8bfe2c4519f8fe27f771
chienphan <chienphan230702@gmail.com>
2024-09-20 14:39:49 +0700

    home ui

--- resources/views/web/home/home.blade.php
+++ resources/views/web/home/home.blade.php
@@ -75,7 +75,7 @@
             <div class="container">
                 <div class="row secondhand-list">
                     @forelse($secondhandProducts as $product)
-                        <div class="col-sm-4 col-md-4 col-lg-3 text-center col-6 py-3 product-item">
+                        <div class="col-custom text-center py-3 product-item">
                             <a href="{{ route('web.productdetail.index', ['slug' => $product->slug]) }}">
                                 <img class="product-img" src="{{ getImage($product->photo) }} " alt="">
                                 @auth
