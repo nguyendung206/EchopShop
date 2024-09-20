3902bc106757ea291c6e9c710087d88a919131bf
chienphan <chienphan230702@gmail.com>
2024-09-20 11:05:33 +0700

    discount

--- resources/views/admin/discount/show.blade.php
+++ resources/views/admin/discount/show.blade.php
@@ -21,7 +21,7 @@
                         </h6>
                         <hr class="mt-0 mb-4">
                         <div class="row pt-1">
-                            <div class="col-6 mb-3">
+                            <div class="col-12 mb-3">
                             <h6>Mô tả</h6>
                             <p class="text-muted">{{strip_tags($discount->description)}}</p>
                             </div>
