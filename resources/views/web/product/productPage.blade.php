46b094ec0b0bb372c41fe42218bcd57ea056bddc
nguyendung206 <moto2062002@gmail.com>
2024-10-28 15:46:23 +0700

    fix bug and UI

--- resources/views/web/product/productPage.blade.php
+++ resources/views/web/product/productPage.blade.php
@@ -167,7 +167,7 @@ Danh sách sản phẩm theo danh mục
                             </a>
                         </div>
                         @break
-                        
+
                         @default
                         @endswitch
                     </div>
@@ -182,26 +182,44 @@ Danh sách sản phẩm theo danh mục
                 @forelse($products as $product)
                 <div class="gift-item m-2 col-lg-4 col-6"
                     style="margin-left: 0px !important;margin-right: 0px !important">
-                    <div style="position: relative;">
-                        <img class="product-img" src="{{ getImage($product->photo) }} " alt="">
-                        @auth
-                        <a href="#"
-                            class='product-heart {{ auth()->user()->load('favorites')->favorites->contains('product_id', $product->id)? 'favorite-active': '' }} '
-                            data-url-destroy="{{ route('favorite.destroy', $product->id) }}"
-                            data-url-store="{{ route('favorite.store') }}" data-productId="{{ $product->id }}"><i
-                                class="fa-{{ auth()->user()->load('favorites')->favorites->contains('product_id', $product->id)? 'solid': 'regular' }} fa-heart fa-heart-home"></i></a>
-                        @else
-                        <a href="{{ route('web.login') }}" class="product-heart"><i class="fa-regular fa-heart fa-heart-home"></i></a>
-                        @endauth
-                    </div>
-                    <div class="gift-name line-clamp-2">
-                        {{ $product->name }}
-                    </div>
-                    <div class="gift-btn-wrap">
-                        <a href="{{ route('web.productdetail.index', ['slug' => $product->slug]) }}"
-                            class="gift-btn">
-                            <i class="fa-solid fa-gift"></i> Nhận quà tặng
+                    <div class="product-wrap">
+                        <a href="{{ route('web.productdetail.index', ['slug' => $product->slug]) }}">
+                            <div style="position: relative;">
+                                <img class="product-img" src="{{ getImage($product->photo) }} " alt="">
+                                @auth
+                                <a href="#"
+                                    class='product-heart {{ auth()->user()->load('favorites')->favorites->contains('product_id', $product->id)? 'favorite-active': '' }} '
+                                    data-url-destroy="{{ route('favorite.destroy', $product->id) }}"
+                                    data-url-store="{{ route('favorite.store') }}" data-productId="{{ $product->id }}"><i
+                                        class="fa-{{ auth()->user()->load('favorites')->favorites->contains('product_id', $product->id)? 'solid': 'regular' }} fa-heart fa-heart-home"></i></a>
+                                @else
+                                <a href="{{ route('web.login') }}" class="product-heart"><i class="fa-regular fa-heart fa-heart-home"></i></a>
+                                @endauth
+                            </div>
+                            <p class="product-name pt-2 line-clamp-2">{{ $product->name }}</p>
+                            <p class="product-brand pt-2 line-clamp-1">Phân loại: <span>{{ $product->brand->name ?? ""}}</span></p>
+                            <p class="price product-price color-B10000 pt-2 line-clamp-1">{{ format_price($product->price) }}</p>
+                            <div class="user-product-wrap d-flex align-items-center">
+                                @if (isset($product->shop))
+                                <img class="mini-avatar mr-2" src="{{getImage($product->shop->logo)}}" alt="">
+                                <div class="user-product ">
+                                    <p class="line-clamp-1">{{$product->shop->name}} &nbsp;<img src="{{asset('/img/icon/doc-top.png')}}" alt="">&nbsp; {{$product->shop->province?->province_name ?? 'Chưa có địa chỉ'}}</p>
+                                </div>
+                                @else
+                                <img src="{{asset("/img/image/logo.png")}}" alt="" class="mini-avatar-admin mr-2">
+                                <div class="user-product " style="width: 77%">
+                                    <p class="line-clamp-1">Sản phẩm của echop</p>
+                                </div>
+                                @endif
+                            </div>
                         </a>
+                        <br>
+                        <div class="gift-btn-wrap">
+                            <a href="{{ route('web.productdetail.index', ['slug' => $product->slug]) }}"
+                                class="gift-btn">
+                                <i class="fa-solid fa-gift"></i> Nhận quà tặng
+                            </a>
+                        </div>
                     </div>
                 </div>
                 @empty
@@ -215,8 +233,8 @@ Danh sách sản phẩm theo danh mục
                 @forelse($products as $product)
                 <div class="col-lg-4 product-item col-6 text-center py-3  ">
                     <div class="product-wrap product-item-category" style="padding-bottom: 10px;">
-                        <a href="{{ route('web.productdetail.index', ['slug' => $product->slug]) }}" >
-                            <div style="position: relative;" >
+                        <a href="{{ route('web.productdetail.index', ['slug' => $product->slug]) }}">
+                            <div style="position: relative;">
                                 <img class="product-img" src="{{ getImage($product->photo) }} " alt="">
                                 @auth
                                 <a href="#"
@@ -250,7 +268,7 @@ Danh sách sản phẩm theo danh mục
                             </div>
                             @endif
                         </div>
-                        <div class="product-actions" >
+                        <div class="product-actions">
                             @if ($product->type->value == TypeProductEnums::EXCHANGE->value)
                             <a class="btn-chat-exchange" href="{{ route('web.productdetail.index', ['slug' => $product->slug]) }}"><i class="fa-regular fa-comment-dots"></i> Chat</a>
                             <a class="btn-buy-exchange" href="{{ route('web.productdetail.index', ['slug' => $product->slug]) }}">Đổi hàng</a>
