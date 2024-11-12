10ff4c2887d564331ca13b293ac0350663d81329
chienphan <chienphan230702@gmail.com>
2024-11-12 10:00:46 +0700

    move code api and add feature forgot password

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -12,6 +12,7 @@ use Carbon\Carbon;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\URL;
 
 class AuthController extends Controller
 {
@@ -69,40 +70,41 @@ class AuthController extends Controller
 
     public function forgotPassword(ApiEmailRequest $request)
     {
-        try {
+        // try {
 
-            $user = User::where('email', $request->email)->first();
-            $token = \Str::random(40);
-            $pin = rand(100000, 999999);
-            $tokenData = [
-                'email' => $request->email,
-                'token' => $token,
-                'pin' => $pin,
-            ];
-            if (ResetPasswordToken::where('email', $request->email)->exists()) {
-                ResetPasswordToken::where('email', $request->email)->update(['token' => $token, 'pin' => $pin]); // update token mới nếu đã có email trong database
+        $user = User::where('email', $request->email)->first();
+        $token = \Str::random(40);
+        $pin = rand(100000, 999999);
+        $tokenData = [
+            'email' => $request->email,
+            'token' => $token,
+            'pin' => $pin,
+        ];
+        if (ResetPasswordToken::where('email', $request->email)->exists()) {
+            ResetPasswordToken::where('email', $request->email)->update(['token' => $token, 'pin' => $pin]); // update token mới nếu đã có email trong database
 
-                $emailJob = new SendForgotPasswordMail($user, $token, $pin, $user->email);
-                dispatch($emailJob);
-            } else {
-                ResetPasswordToken::create($tokenData);
-                $emailJob = new SendForgotPasswordMail($user, $token, $pin, $user->email);
-                dispatch($emailJob);
-            }
-
-            return response()->json([
-                'success' => true,
-                'email' => $user->email,
-                'token_data' => $token,
-                'pin' => $pin,
-            ], 200);
-        } catch (\Exception $e) {
-            return response()->json([
-                'success' => false,
-                'message' => 'Lỗi server',
-                'error' => $e,
-            ], 500);
+            $emailJob = new SendForgotPasswordMail($user, $token, $pin, $user->email);
+            dispatch($emailJob);
+        } else {
+            ResetPasswordToken::create($tokenData);
+            $emailJob = new SendForgotPasswordMail($user, $token, $pin, $user->email);
+            dispatch($emailJob);
         }
+
+        return response()->json([
+            'success' => true,
+            'email' => $user->email,
+            'token_data' => $token,
+            'pin' => $pin,
+            'url_reset_password' => URL::temporarySignedRoute('api.resetPassword', now()->addMinute(10), ['token' => $token]),
+        ], 200);
+        // } catch (\Exception $e) {
+        //     return response()->json([
+        //         'success' => false,
+        //         'message' => 'Lỗi server',
+        //         'error' => $e,
+        //     ], 500);
+        // }
     }
 
     public function checkPinCodeAndResetPassword(ApiResetPasswordRequest $request, $token)
