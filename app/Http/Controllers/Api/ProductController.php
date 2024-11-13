2ee48329ba1acbfff7ce65ec62a90deb19ea8e91
chienphan <chienphan230702@gmail.com>
2024-11-13 10:52:10 +0700

    push remote

---
+++ app/Http/Controllers/Api/ProductController.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\ApiProductRequest;
+use App\Services\ProductService;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+
+    protected $productService;
+
+
+    public function __construct(ProductService $productService)
+    {
+        $this->productService = $productService;
+    }
+    
+    public function store(ApiProductRequest $request)
+    {
+        try {
+            $product = $this->productService->createProduct($request);
+            if ($product) {
+                if ($request->unittype == 1) {
+                    $quantity = $request->quantity;
+
+                    $this->productService->createProductUnit([
+                        'type' => $request->unittype,
+                        'product_id' => $product->id,
+                        'color' => null,
+                        'size' => null,
+                        'quantity' => $quantity > 0 ? $quantity : 1,
+                    ]);
+                } elseif ($request->unittype == 2) {
+                    $colors = $request->input('colors', []);
+                    $sizes = $request->input('sizes', []);
+                    $quantities = $request->input('quantities', []);
+
+                    foreach ($quantities as $index => $quantity) {
+                        $this->productService->createProductUnit([
+                            'type' => $request->unittype,
+                            'product_id' => $product->id,
+                            'color' => $colors[$index] ?? '',
+                            'size' => $sizes[$index] ?? '',
+                            'quantity' => $quantity > 0 ? $quantity : 1,
+                        ]);
+                    }
+                }
+
+                return response()->json([
+                    'status' => 200,
+                    'success' => true, 'message' => 'Sản phẩm đã được thêm thành công!'
+                ], 200);
+            } else {
+                return response()->json([
+                    'status' => 500,
+                    'message' => 'Đăng bài thất bại.'
+                ], 500);
+            }
+        } catch (\Exception $e) {
+            return response()->json([
+                'status' => 500,
+                'message' => 'Đã có lỗi xảy ra, vui lòng thử lại!',
+                'error' => $e,
+            ], 500);
+        }
+    }
+}
